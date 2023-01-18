5e6a0be25bc640beba11693cd8dd97f0e660c8b8
freemile2ru <sofiyullahj@gmail.com>
2023-01-18 20:22:48 +0000

    temp

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 fetch("https://dummyjson.com/users")
-.then(res => {
-  return res.json();
-})
+  .then((res) => {
+    return res.json();
+  })
 
-.then(data =>{
-  let i= 0
-  data.users.forEach(user => 
-{i++; if (i <= 12)
-
-{
-const newUser = document.getElementById("users")
-// const newClass = document.querySelector()
-const userData = document.createElement("div")
-userData.innerHTML = `<div class="user">
+  .then((data) => {
+    console.log("bjf", data);
+    let i = 0;
+    data.users.forEach((user) => {
+      i++;
+      if (i <= 12) {
+        const newUser = document.getElementById("users");
+        // const newClass = document.querySelector()
+        const userData = document.createElement("div");
+        userData.innerHTML = `<div class="user">
 <h1 class="name">${user.firstName} ${user.lastName}</h1>
 <img src="${user.image}" alt="img">
 <p class="gender">${user.gender}</p>
 <p class="username">${user.username}</p>
 </div>`;
-// const newContent= document.importNode(newClass)
-newUser.appendChild(userData);
-}}
-)
-  }
-)
+        // const newContent= document.importNode(newClass)
+        newUser.appendChild(userData);
+      }
+    });
+  });
 
-    
 // document.getElementById(userData)
 //   })}
 //   =>{
 //   data.forEach(user =>{
 
-  // })
+// })
 // }
 //   ,
-   
-
-  
 
 // )
 // .catch(error => {
 //   console.log(error)
 // });
 // array.forEach(element => {
-  
+
 // });
